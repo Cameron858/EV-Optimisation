e4328f2c806b29b6d5d21912b5bc3263da68c6cb
Cameron Wood <cameron.wood@capgemini.com>
2025-02-06 10:43:52 +0000

    Added tests for mutations.

--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -47,3 +47,51 @@ def test_that_mutation_rate_raises_value_error_where_applicable(rate, raises):
             )
     else:
         v.mutate(rate=rate)
+
+
+def test_that_zero_mutation_rate_produces_no_mutations():
+    """This test also checks that derived attributes such as component weights are updated."""
+    v = Vehicle(100, 100, 100)
+
+    before_motor_weight = v.motor_weight
+    before_battery_weight = v.battery_weight
+    before_motor_power = v.motor_power
+    before_battery_capacity = v.battery_capacity
+    before_frame_weight = v.frame_weight
+
+    v.mutate(0)
+
+    # core attributes
+    assert v.motor_power == before_motor_power
+    assert v.battery_capacity == before_battery_capacity
+    assert v.frame_weight == before_frame_weight
+
+    # derived attributes
+    assert v.motor_weight == before_motor_weight
+    assert v.battery_weight == before_battery_weight
+
+
+def test_that_one_mutation_rate_always_produces_mutations(mocker):
+    """This test also checks that derived attributes such as component weights are updated."""
+
+    # mutated values will be *2 the original
+    mocker.patch("random.uniform", return_value=1)
+
+    v = Vehicle(100, 100, 100)
+
+    before_motor_weight = v.motor_weight
+    before_battery_weight = v.battery_weight
+    before_motor_power = v.motor_power
+    before_battery_capacity = v.battery_capacity
+    before_frame_weight = v.frame_weight
+
+    v.mutate(1)
+
+    # core attributes
+    assert v.motor_power == 2 * before_motor_power
+    assert v.battery_capacity == 2 * before_battery_capacity
+    assert v.frame_weight == 2 * before_frame_weight
+
+    # derived attributes
+    assert v.motor_weight == 2 * before_motor_weight
+    assert v.battery_weight == 2 * before_battery_weight

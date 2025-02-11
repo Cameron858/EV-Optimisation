fd2b97dbdd34b08d41db937ae16aa87bef5fa0f7
Cameron <cameronwood858@googlemail.com>
2025-02-11 20:48:53 +0000

    Added function for calculating the coeff of rolling resistance.

--- src/ev_optimisation/model.py
+++ src/ev_optimisation/model.py
@@ -6,6 +6,20 @@ def rpm_to_rads(rpm):
     return (rpm * 2 * math.pi) / 60
 
 
+def coeff_rolling_resistance(tire_pressure: float, velocity: float):
+    """Calculate the coefficient of rolling resistance.
+
+    Parameters
+    -----------
+    tire_pressure : float
+        Tire pressure in [bar]
+    velocity : float
+        Velocity of vehicle in [kmh-1]
+    """
+    velocity_term = 0.0095 * (velocity / 100) ** 2
+    return 0.005 + (1 / tire_pressure) * (0.01 + velocity_term)
+
+
 def rolling_resistance_force(c: float, m: float, g=9.81):
     """Calculate the rolling resistance force in N.
 

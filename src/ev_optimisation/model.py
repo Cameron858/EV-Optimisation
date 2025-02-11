df641b67eec4cec7da2da8b74adbfbbd51ba76cc
Cameron <cameronwood858@googlemail.com>
2025-02-11 20:43:37 +0000

    Added function to calculate the rolling resistance force.

--- src/ev_optimisation/model.py
+++ src/ev_optimisation/model.py
@@ -4,3 +4,18 @@ import math
 def rpm_to_rads(rpm):
     """Convert RPM to rads-1"""
     return (rpm * 2 * math.pi) / 60
+
+
+def rolling_resistance_force(c: float, m: float, g=9.81):
+    """Calculate the rolling resistance force in N.
+
+    Parameters
+    -----------
+    c : float
+        rolling resistance coefficient
+    m : float
+        mass in [kg]
+    g : float = 9.81
+        acceleration due to gravity [ms-2]
+    """
+    return c * m * g

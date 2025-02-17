1c6ab0b35ef42bfafb135c3007aed2fb813c205c
Cameron <cameronwood858@googlemail.com>
2025-02-17 19:24:54 +0000

    Updated tests.

--- tests/test_vehicle.py
+++ tests/test_vehicle.py
@@ -6,7 +6,7 @@ from ev_optimisation.vehicle import Vehicle
 def test_that_motor_weight_is_calculated_correctly_on_init(power, expected_weight):
 
     # 100 has been chosen as an arbitary value
-    v = Vehicle(power, 100, 100)
+    v = Vehicle(power, 100)
 
     assert v.motor_weight == expected_weight
 
@@ -15,7 +15,7 @@ def test_that_motor_weight_is_calculated_correctly_on_init(power, expected_weigh
 def test_that_battery_weight_is_calculated_correctly_on_init(capacity, expected_weight):
 
     # 100 has been chosen as an arbitary value
-    v = Vehicle(100, capacity, 100)
+    v = Vehicle(100, capacity)
 
     assert v.battery_weight == expected_weight
 
@@ -36,7 +36,7 @@ def test_that_battery_weight_is_calculated_correctly_on_init(capacity, expected_
 )
 def test_that_mutation_rate_raises_value_error_where_applicable(rate, raises):
 
-    v = Vehicle(100, 100, 100)
+    v = Vehicle(100, 100)
 
     if raises:
         with pytest.raises(ValueError) as exc_info:
@@ -50,11 +50,8 @@ def test_that_mutation_rate_raises_value_error_where_applicable(rate, raises):
 
 
 def test_that_zero_mutation_rate_produces_no_mutations():
-    """This test also checks that derived attributes such as component weights are updated."""
-    v = Vehicle(100, 100, 100)
+    v = Vehicle(100, 100)
 
-    before_motor_weight = v.motor_weight
-    before_battery_weight = v.battery_weight
     before_motor_power = v.motor_power
     before_battery_capacity = v.battery_capacity
 
@@ -64,10 +61,6 @@ def test_that_zero_mutation_rate_produces_no_mutations():
     assert v.motor_power == before_motor_power
     assert v.battery_capacity == before_battery_capacity
 
-    # derived attributes
-    assert v.motor_weight == before_motor_weight
-    assert v.battery_weight == before_battery_weight
-
 
 def test_that_one_mutation_rate_always_produces_mutations(mocker):
     """This test also checks that derived attributes such as component weights are updated."""
@@ -75,10 +68,8 @@ def test_that_one_mutation_rate_always_produces_mutations(mocker):
     # mutated values will be *2 the original
     mocker.patch("random.uniform", return_value=1)
 
-    v = Vehicle(100, 100, 100)
+    v = Vehicle(100, 100)
 
-    before_motor_weight = v.motor_weight
-    before_battery_weight = v.battery_weight
     before_motor_power = v.motor_power
     before_battery_capacity = v.battery_capacity
 
@@ -87,7 +78,3 @@ def test_that_one_mutation_rate_always_produces_mutations(mocker):
     # core attributes
     assert v.motor_power == 2 * before_motor_power
     assert v.battery_capacity == 2 * before_battery_capacity
-
-    # derived attributes
-    assert v.motor_weight == 2 * before_motor_weight
-    assert v.battery_weight == 2 * before_battery_weight

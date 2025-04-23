3a394f5f6cf79eb910098e3d763b684cabe18756
Cameron <cameronwood858@googlemail.com>
2025-04-23 13:05:22 +0100

    Refactor mutation method in Vehicle class to use polynomial mutation for attributes

--- src/ev_optimisation/vehicle.py
+++ src/ev_optimisation/vehicle.py
@@ -1,6 +1,7 @@
 import logging
 import random
 from dataclasses import dataclass, field
+from .operators import polynomial_mutation
 
 type kg = float | int
 type kW = float | int
@@ -13,6 +14,10 @@ class Vehicle:
     # This defines the +-% that a mutation can possible change by
     # i.e. mutated_x = x * (1 + uniform(-0.1, 0.1))
     MUTATION_PERC_CHANGE: float = field(default=0.05, init=False)
+
+    BATTERY_CAPACITY_BOUNDS: tuple[float] = field(default=(30, 150), init=False)  # kWh
+    MOTOR_WEIGHT_BOUNDS: tuple[float] = field(default=(50, 500), init=False)  # kW
+
     BATTERY_WEIGHT_RATIO: float = field(default=6, init=False)  # kg/kWh
     MOTOR_WEIGHT_RATIO: float = field(default=4, init=False)  # kg/kW
 
@@ -31,17 +36,24 @@ class Vehicle:
     def mass(self) -> kg:
         return self.motor_weight + self.battery_weight
 
-    def mutate(self, rate: float) -> None:
-        """Mutate the vehicle's attributes independently based on a mutation rate."""
+    def mutate(self, rate: float, eta: int = 5) -> None:
+        """Mutate the vehicle's attributes inplace independently using polynomial mutation.
+
+        Parameters
+        -----------
+        eta : int, optional
+            The distribution index that controls the extent of the mutation.
+            Higher values of `eta` result in smaller mutations. Default is 5.
+        """
         if not 0 <= rate <= 1:
             raise ValueError(f"Mutation rate must be in range [0, 1]. Given: {rate}")
 
         if random.random() < rate:
-            self.motor_power *= 1 + random.uniform(
-                -self.MUTATION_PERC_CHANGE, self.MUTATION_PERC_CHANGE
+            self.motor_power = polynomial_mutation(
+                self.motor_power, self.MOTOR_WEIGHT_BOUNDS
             )
 
         if random.random() < rate:
-            self.battery_capacity *= 1 + random.uniform(
-                -self.MUTATION_PERC_CHANGE, self.MUTATION_PERC_CHANGE
+            self.battery_capacity = polynomial_mutation(
+                self.battery_capacity, self.BATTERY_CAPACITY_BOUNDS
             )

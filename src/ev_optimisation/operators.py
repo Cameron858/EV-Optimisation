e4d80af83c52541ab58581f78d2de2829f1418b0
Cameron <cameronwood858@googlemail.com>
2025-04-23 13:05:10 +0100

    Add polynomial mutation function for genetic algorithm

--- src/ev_optimisation/operators.py
+++ src/ev_optimisation/operators.py
@@ -39,3 +39,38 @@ def crossover(
             parent_1.battery_capacity, parent_2.battery_capacity, **operator_kwargs
         ),
     )
+
+
+def polynomial_mutation(x: float, bounds: tuple, eta=5) -> float:
+    """
+    Perform polynomial mutation on a given real encoded value.
+
+    Clip mutated value to `bounds`.
+
+    Parameters
+    ----------
+    x : float
+        The input value to be mutated.
+    bounds : tuple
+        A tuple specifying the lower and upper bounds for the mutation
+        (bounds[0] is the lower bound, bounds[1] is the upper bound).
+    eta : int, optional
+        The distribution index that controls the extent of the mutation.
+        Higher values of `eta` result in smaller mutations. Default is 5.
+
+    Returns
+    -------
+    float
+        The mutated value.
+    """
+    u = random.uniform(0, 1)
+    exponent = 1 / (1 + eta)
+    if u < 0.5:
+        delta = (2 * u) ** exponent - 1
+    else:
+        delta = 1 - (2 * (1 - u)) ** exponent
+
+    x_mutated = x + delta * (bounds[1] - bounds[0])
+    x_mutated = max(bounds[0], min(bounds[1], x_mutated))
+
+    return x_mutated

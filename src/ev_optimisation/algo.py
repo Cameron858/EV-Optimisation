3c899376985ffe888388a6c59b46fd78ee68a1e2
Cameron <cameronwood858@googlemail.com>
2025-05-09 13:27:54 +0100

    Refactor vehicle creation logic by extracting create_random_vehicle function and simplifying create_population implementation

--- src/ev_optimisation/algo.py
+++ src/ev_optimisation/algo.py
@@ -5,9 +5,27 @@ from ev_optimisation.vehicle import Vehicle, VehicleConfig
 import numpy as np
 
 
+def create_random_vehicle() -> Vehicle:
+    """
+    Create a single Vehicle instance with random motor power and battery capacity.
+
+    Returns
+    -------
+    Vehicle
+        A Vehicle instance with randomly generated attributes.
+    """
+    power = random.uniform(*Vehicle.MOTOR_POWER_BOUNDS)
+    power = round(power, 2)
+
+    capacity = random.uniform(*Vehicle.BATTERY_CAPACITY_BOUNDS)
+    capacity = round(capacity, 2)
+
+    return Vehicle(motor_power=power, battery_capacity=capacity)
+
+
 def create_population(size: int) -> list[Vehicle]:
     """
-    Creates a population of Vehicle instances with random motor power and battery capacity.
+    Create a population of Vehicle instances with random attributes.
 
     Parameters
     ----------
@@ -19,19 +37,7 @@ def create_population(size: int) -> list[Vehicle]:
     list[Vehicle]
         A list of Vehicle instances with randomly generated attributes.
     """
-    p = []
-    for _ in range(size):
-
-        power = random.uniform(*Vehicle.MOTOR_POWER_BOUNDS)
-        power = round(power, 2)
-
-        capacity = random.uniform(*Vehicle.BATTERY_CAPACITY_BOUNDS)
-        capacity = round(capacity, 2)
-
-        v = Vehicle(motor_power=power, battery_capacity=capacity)
-        p.append(v)
-
-    return p
+    return [create_random_vehicle() for _ in range(size)]
 
 
 def pareto_dominance(i1: np.ndarray, i2: np.ndarray) -> bool:

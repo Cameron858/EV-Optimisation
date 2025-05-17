f6b189237ef07918e8d4fe56f8afd6217e0029b5
Cameron <cameronwood858@googlemail.com>
2025-05-17 23:06:49 +0100

    Created initial app layout.

--- app/app.py
+++ app/app.py
@@ -1,6 +1,5 @@
-from dash import Dash, html
+from dash import Dash, html, dcc
 import dash_bootstrap_components as dbc
-import logging
 
 app = Dash(
     __name__,
@@ -9,4 +8,71 @@ app = Dash(
     suppress_callback_exceptions=True,
 )
 
-app.layout = html.Div([html.H1("Hello world!")])
+app.layout = html.Div(
+    [
+        dbc.NavbarSimple(brand="EV Optimiser", color="lightseagreen"),
+        dbc.Row(
+            [
+                # left - inputs
+                dbc.Col(
+                    [
+                        dbc.Card(
+                            [
+                                dbc.CardBody(
+                                    [
+                                        html.H3(
+                                            "Parameters",
+                                            className="card-title",
+                                        ),
+                                        dbc.Row(
+                                            [
+                                                dbc.Label(
+                                                    "Population size",
+                                                    html_for="pop-size-input",
+                                                ),
+                                                dbc.Input(
+                                                    type="number",
+                                                    id="pop-size-input",
+                                                    placeholder="Population size",
+                                                    value=10,
+                                                    min=2,
+                                                    max=50,
+                                                ),
+                                            ]
+                                        ),
+                                        dbc.Row(
+                                            [
+                                                dbc.Label(
+                                                    "Generations",
+                                                    html_for="n-gens-input",
+                                                ),
+                                                dbc.Input(
+                                                    type="number",
+                                                    id="n-gens-input",
+                                                    placeholder="Number of generations",
+                                                    value=10,
+                                                    min=1,
+                                                    max=50,
+                                                ),
+                                            ]
+                                        ),
+                                    ]
+                                )
+                            ],
+                            class_name="m-1",
+                        ),
+                        dbc.Button(
+                            "Run Optimiser",
+                            id="run-btn",
+                            class_name="m-1 btn-lightseagreen",
+                        ),
+                    ],
+                    # takes up 1/3
+                    width=4,
+                ),
+                # right - outputs
+                dbc.Col([dcc.Graph(id="main-output-graph")]),
+            ]
+        ),
+    ]
+)
